130ceb991e8b8cb435afd6980050edc12bfc9494
CORTEZZ <sorikson083@gmail.com>
2024-05-05 14:07:34 +0500

    contract done / add react components / add "express" "dotenv" dependencies

--- server/contracts/BlockStore.sol
+++ server/contracts/BlockStore.sol
@@ -1,82 +1,166 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract CStore {
-    enum Roles {
+contract BlockStore {
+    enum Role {
         Admin,
-        Supplier,
-        Buyer,
         Seller,
-        Store
+        Buyer,
+        Supplier
+    }
+
+    enum ReqStatus {
+        Approved,
+        Rejected,
+        Waiting
+    }
+
+    struct User {
+        Role role;
+        address userAddress;
+        string password;
+        bool isRegistered;
+    }
+
+    struct Store {
+        address owner;
+        string name;
+        uint balance;
     }
 
     struct Product {
         uint id;
         string name;
-        address supplier;
         uint price;
-        uint numberOfProducts;
+        address seller;
+        bool isAvailable;
     }
 
-    struct Purchase {
-        address buyer;
+    struct RoleChangeRequest {
+        address userAddress;
+        Role currentRole;
+        Role desiredRole;
+        ReqStatus reqStatus;
+    }
+
+    struct DeliveryRequest {
+        address sender;
+        address receiver;
         uint productId;
-        uint numberOfProducts;
+        bool isDelivered;
     }
 
-    struct Supplier {
-        address addr;
-        int rating;
-        uint[] productIds;
+    mapping(address => User) public users;
+    mapping(address => Store) public stores;
+    mapping(uint => Product) public products;
+    
+    mapping(address => RoleChangeRequest) public roleChangeRequests;
+    mapping(uint => DeliveryRequest) public deliveryRequests;
+
+    uint public productCount;
+    uint public requestCount;
+
+    constructor() {
+        
     }
 
-    mapping (address => Roles) public users;
-    mapping (address => Supplier) public suppliers;
-    mapping (uint => Product) public products;
-    mapping (uint => Purchase) public purchases;
-
-    // +
-    function createProduct(uint _id, string memory _name, uint _price) public {
-        require(bytes(_name).length > 0, "The name was entered incorrectly!");
-        require(_price > 0, "The price was entered incorrectly!");
-        require(products[_id].id != _id, "A product with this ID already exists!");
-        require(msg.sender == suppliers[msg.sender].addr, "You are not a supplier!");
-        products[_id] = Product(_id, _name, msg.sender, _price, 0);
+    modifier onlyAdmin() {
+        require(users[msg.sender].role == Role.Admin, "Only admin can perform this action");
+        _;
     }
 
-    // +
-    function addSupplier(address _addr, int _rating) public {
-        require(_addr != suppliers[msg.sender].addr, "A supplier with this address already exists!");
-        Supplier memory newSupplier = Supplier(_addr, _rating, new uint[](0));
-        suppliers[_addr] = newSupplier;
+    modifier onlySeller() {
+        require(users[msg.sender].role == Role.Seller, "Only seller can perform this action");
+        _;
     }
 
-    // +
-    function shipProduct(uint _productId, uint _numberOfProducts) public {
-        require(products[_productId].id != 0, "The product doesn't exist!");
-        require(products[_productId].supplier == msg.sender, "You are not a supplier!");
-        products[_productId].numberOfProducts = products[_productId].numberOfProducts + _numberOfProducts;
+    modifier onlyBuyer() {
+        require(users[msg.sender].role == Role.Buyer, "Only buyer can perform this action");
+        _;
     }
 
-    // +
-    function sellProduct(uint _purchaseId, address _buyer, uint _productId, uint _numberOfProducts) public {
-        require(products[_productId].id != 0, "There is no such product in the store!");
-        require(products[_productId].numberOfProducts - _numberOfProducts > 0, "The product is not enough!");
-        Purchase memory newPurchase = Purchase(_buyer, _productId, _numberOfProducts);
-        purchases[_purchaseId] = newPurchase;
-        products[_productId].numberOfProducts = products[_productId].numberOfProducts - _numberOfProducts;
+    modifier onlySupplier() {
+        require(users[msg.sender].role == Role.Supplier, "Only supplier can perform this action");
+        _;
     }
 
-    // +
-    function returnProduct(uint _purchaseId) public {
-        require(purchases[_purchaseId].buyer != address(0), "The purchase doesn't exist");
-        require(purchases[_purchaseId].buyer == msg.sender, "The purchase was not made by you!");
-        require(purchases[_purchaseId].productId == _purchaseId, "You have not bought this product!");
-        products[purchases[_purchaseId].productId].numberOfProducts = products[purchases[_purchaseId].productId].numberOfProducts + purchases[_purchaseId].numberOfProducts;
-        delete purchases[_purchaseId];
+    function createUser(address _userAddress, Role _role, string memory _password) public {
+        require(!users[_userAddress].isRegistered, "User already registered");
+        users[_userAddress] = User(_role, _userAddress, _password, true);
     }
 
-    function updateRole(address _addr, Roles _role) public {
-        users[_addr] = _role;
+    function createStore(string memory _name) public onlyAdmin {
+        stores[msg.sender] = Store(msg.sender, _name, 0);
+    }
+
+    function createAdmin(address _adminAddress, string memory _password) public onlyAdmin {
+        require(!users[_adminAddress].isRegistered, "User already registered, update his role");
+        users[_adminAddress] = User(Role.Admin, _adminAddress, _password, true);
+    }
+
+    function removeStore(address _storeAddress) public onlyAdmin {
+        require(stores[_storeAddress].owner != address(0), "Store does not exist");
+        delete stores[_storeAddress];
+
+        users[_storeAddress].role = Role.Buyer;
+    }
+
+    function createProduct(string memory _name, uint _price) public onlySeller {
+        productCount++;
+        products[productCount] = Product(productCount, _name, _price, msg.sender, true);
+    }
+
+    function buyProduct(uint _productId) public payable onlyBuyer {
+        Product memory product = products[_productId];
+        require(product.id != 0, "Product doesn't exist");
+        require(product.isAvailable, "Product is not available");
+        require(msg.value >= product.price, "Not enough Ether provided");
+
+        payable(product.seller).transfer(msg.value);
+        product.isAvailable = false;
+    }
+
+    function returnProduct(uint _productId) public payable onlyBuyer {
+        Product storage product = products[_productId];
+        require(product.isAvailable, "Product is not available for return");
+
+        payable(msg.sender).transfer(product.price);
+
+        product.isAvailable = true;
+    }
+
+
+    function createRequestRoleChange(Role _desiredRole) public {
+        require(users[msg.sender].role != _desiredRole, "You already have the desired role");
+        roleChangeRequests[msg.sender] = RoleChangeRequest(msg.sender, users[msg.sender].role, _desiredRole, ReqStatus.Waiting);
+    }
+
+    function approveRoleChange(address _userAddress) public onlyAdmin {
+        RoleChangeRequest storage request = roleChangeRequests[_userAddress];
+        require(request.reqStatus == ReqStatus.Waiting, "The application has already been reviewed");
+        users[_userAddress].role = request.desiredRole;
+        request.reqStatus = ReqStatus.Approved;
+    }
+
+    function rejectRoleChange(address _userAddress) public onlyAdmin {
+        RoleChangeRequest storage request = roleChangeRequests[_userAddress];
+        require(request.reqStatus == ReqStatus.Waiting, "The application has already been reviewed");
+        request.reqStatus = ReqStatus.Rejected;
+    }
+
+    function createDeliveryRequest(address _receiver, uint _productId) public {
+        requestCount++;
+        deliveryRequests[requestCount] = DeliveryRequest(msg.sender, _receiver, _productId, false);
+    }
+
+    function confirmDelivery(uint _requestId) public {
+        DeliveryRequest storage request = deliveryRequests[_requestId];
+        Product storage product = products[request.productId];
+        
+        require(msg.sender == request.sender || msg.sender == request.receiver, "Only sender or receiver can confirm delivery");
+        require(product.isAvailable, "Product is not available for return");
+
+        product.isAvailable = true;
+        request.isDelivered = true;
     }
 }
